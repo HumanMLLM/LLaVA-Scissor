a714cb6af5545a908bb85010e989f99295f8c249
BB CHAN <sbysbysby123@gmail.com>
2025-05-29 11:24:20 +0800

    Add files via upload

---
+++ scripts/eval/eval_lmms.sh
@@ -0,0 +1,19 @@
+
+
+MODEL_NAME="llava-ov-7B-bin_llava558k_pt_flat_zip_baseline"
+MODEL_PATH='checkpoints/llava-onevision--siglip-so400m-patch14-384-Qwen2.5-7B-Instruct-llava558k-pt-oryx-ft-flat'
+
+# MODEL_NAME="llava-ov-0.5B-bin_llava558k_pt_oryx_flat_baseline"
+# MODEL_PATH='checkpoints/llava-onevision-siglip-so400m-patch14-384-Qwen2.5-0.5B-Instruct-llava558k-pt-oryx-ft-flat'
+
+accelerate launch --num_processes 8 --main_process_port 23553 -m lmms_eval \
+    --model llava_onevision \
+    --model_args pretrained=$MODEL_PATH,conv_template=qwen_2,model_name=llava_qwen_zip \
+    --tasks mvbench \
+    --batch_size 1 \
+    --log_samples \
+    --log_samples_suffix eval \
+    --output_path ./logs/$MODEL_NAME
+
+echo $MODEL_NAME
+echo $MODEL_PATH
